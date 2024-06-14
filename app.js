da0c15157c0875da69b65a64240aa4095a463461
gagansuryanagara <gagansuryanagara@gmail.com>
2024-06-14 21:18:52 +0700

    install ejs, setting ejs, pisah view

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express   = require('express')
 const app       = express()
 const port      = 3000
 
+app.set('view engine', 'ejs') // setting penggunaan template engine untuk express
+app.set('views', './view-ejs') // setting penggunaan folder untuk mengyompan file .ejs
+
+
 app.get('/',(req,res)=>{
-    res.send('<h1>Hello World</h1><hr>')
+    res.render('index.ejs')
 }) 
 
 app.get('/profil',(req,res)=>{
-    res.send('<h1>Profil Saya</h1>')
+    res.render('profil.ejs')
 })
 
 app.get('/alamat',(req,res)=>{
-    res.send('<h1>Alamat</h1>')
+    res.render('alamat.ejs')
 }) 
 
 app.listen(port, ()=>{
